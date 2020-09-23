626f1fcad0ac748b2ca6fb60f0df7ddeb6116172
40823237 <40823237@gm.nfu.edu.tw>
2020-09-23 13:03:30 +0800

    check title

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'Course', 'text': '', 'tags': '', 'url': 'Course.html'}, {'title': 'W1', 'text': '', 'tags': '', 'url': 'W1.html'}, {'title': 'HW', 'text': '', 'tags': '', 'url': 'HW.html'}, {'title': 'Note', 'text': '', 'tags': '', 'url': 'Note.html'}]};
+var tipuesearch = {"pages": [{'title': 'Course', 'text': '', 'tags': '', 'url': 'Course.html'}, {'title': 'W1', 'text': '建立網頁、模組 \n', 'tags': '', 'url': 'W1.html'}, {'title': 'HW', 'text': '', 'tags': '', 'url': 'HW.html'}, {'title': 'Note', 'text': '', 'tags': '', 'url': 'Note.html'}]};
